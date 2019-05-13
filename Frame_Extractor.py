869e12b0572eaa96d333763a0a956561879cfd15
Johan Andrey Bosso <bossojohan@gmail.com>
2019-05-13 09:50:05 +0200

    Frame_extractor.py in progress

--- Frame_Extractor.py
+++ Frame_Extractor.py
@@ -1,6 +1,5 @@
-import os, os.path, csv, json, glob
+import os, os.path, csv
 import numpy as np
-from pprint import pprint
 from subprocess import call
 from Utility import Annotation 
              
@@ -9,9 +8,16 @@ def main():
     annotation = Annotation()
            
     with open(os.path.join(main_root,'SoccerNet_V1.1_Labels.csv'))as annotation_list:
-        reader = csv.reader(annotation_list, delimiter = ',')        
+        reader_files = csv.reader(annotation_list, delimiter = ',')        
         for row in annotation_list:
-            print (row)
+            src, game_code, _ = row.split(',') 
+           
+            with open(os.path.join(main_root+ '/'+ src)) as annotation_file:
+                reader_notes = csv.reader(annotation_file, delimiter = ',')
+                for note in annotation_file:
+                    
+                
+            
                       
         
 if __name__ == '__main__' :
